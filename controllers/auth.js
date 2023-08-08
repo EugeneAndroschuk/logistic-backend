aff5759d928bb5996f27cf34f8fa2683be49133b
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-08-09 01:17:20 +0300

    fix frontend url

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const { JWT_SECRET, FRONTEND_URL } = process.env;
 
 const googleAuth = (req, res) => {
-  const url = `${FRONTEND_URL}?token=${req.user.token}`;
+  const url = `${FRONTEND_URL}/auth?token=${req.user.token}`;
 
   res.redirect(url);
 };
